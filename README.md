3c1b672016ac554907d1883ba11e038b4b0861a1
nunezada08 <vitor.nunes3013@gmail.com>
2025-05-23 09:01:10 -0300

    feat: usa metodo push e adiciona arquivo README.md

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
 <h1 align="center">Aulas com exemplos de metodos array</h1>
+
+<h2 align="center">Método push:</h2>
+<h3 align="center">Adiciona elementos ao final da array</h3>
---
+++ script/metodo-push.js
@@ -0,0 +1,10 @@
+let playlist = ["funk", "hip-hop", "pop"]
+
+console.log("Antes do push:");
+console.table(playlist)
+
+//adicionando novo estilo
+playlist.push("Trap");
+
+console.log("depois do push:")
+console.table(playlist)
